46e61201e154be03b748934ed3fa17ca2ded6567
giana medeiros <gianamedeiros.00510@gmail.com>
2023-05-05 19:46:41 -0300

    Dev profile

--- src/main/java/com/workshop/workshop/system/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/workshop/workshop/system/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.workshop.workshop.system.resources.exceptions;
 
+import com.workshop.workshop.system.services.exceptions.DatabaseException;
 import com.workshop.workshop.system.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,6 +17,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> ResourceNotFound (ResourceNotFoundException e, HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database (DatabaseException e, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/workshop/workshop/system/services/UserService.java
+++ src/main/java/com/workshop/workshop/system/services/UserService.java
@@ -2,8 +2,12 @@ package com.workshop.workshop.system.services;
 
 import com.workshop.workshop.system.entities.User;
 import com.workshop.workshop.system.repositories.UserRepository;
+import com.workshop.workshop.system.services.exceptions.DatabaseException;
 import com.workshop.workshop.system.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,12 +32,22 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+           throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity,obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity,obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
